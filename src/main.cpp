38c80edeccc5f664a5d7295d25316583d9bcc764
qiushao <qiushaox@gmail.com>
2026-01-06 11:17:28 +0800

    设置 Fusion style， 统一各平台的 ui 风格

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <QApplication>
+#include <QStyleFactory>
 #include "ui/MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qDebug() << "style list:" << QStyleFactory::keys();
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
     MainWindow w;
     w.show();
     return QApplication::exec();
