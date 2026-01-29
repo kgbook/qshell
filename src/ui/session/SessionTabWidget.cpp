14d81b65c65ef58ce3dd38691471156fb2715ea0
qiushao <qiushaox@gmail.com>
2026-01-29 14:32:35 +0800

    fixed crash when close tab

--- src/ui/session/SessionTabWidget.cpp
+++ src/ui/session/SessionTabWidget.cpp
@@ -7,25 +7,12 @@
 SessionTabWidget::SessionTabWidget(QWidget *parent)
     : QTabWidget(parent) {
 
-    // 设置 tab 可关闭
-    setTabsClosable(true);
-
     // 为 tab bar 启用右键菜单
     tabBar()->setContextMenuPolicy(Qt::CustomContextMenu);
 
     // 连接信号
     connect(tabBar(), &QTabBar::customContextMenuRequested,
             this, &SessionTabWidget::showTabContextMenu);
-
-    // 连接关闭按钮信号
-    connect(this, &QTabWidget::tabCloseRequested,
-            this, [this](int index) {
-                QWidget *widget = this->widget(index);
-                removeTab(index);
-                if (widget) {
-                    widget->deleteLater();
-                }
-            });
 }
 
 void SessionTabWidget::showTabContextMenu(const QPoint &pos) {
@@ -42,11 +29,6 @@ void SessionTabWidget::showTabContextMenu(const QPoint &pos) {
 
     QAction *renameAction = menu.addAction(tr("Rename Tab"));
     connect(renameAction, &QAction::triggered, this, &SessionTabWidget::renameTab);
-
-    menu.addSeparator();
-
-    QAction *closeAction = menu.addAction(tr("Close Tab"));
-    connect(closeAction, &QAction::triggered, this, &SessionTabWidget::closeTab);
 
     // 在鼠标位置显示菜单
     menu.exec(tabBar()->mapToGlobal(pos));
@@ -72,20 +54,5 @@ void SessionTabWidget::renameTab() {
     // 如果用户确认且输入不为空，则重命名
     if (ok && !newName.isEmpty()) {
         setTabText(m_contextMenuTabIndex, newName);
-    }
-}
-
-void SessionTabWidget::closeTab() {
-    if (m_contextMenuTabIndex < 0 || m_contextMenuTabIndex >= count()) {
-        return;
-    }
-
-    // 获取并移除 tab
-    QWidget *widget = this->widget(m_contextMenuTabIndex);
-    removeTab(m_contextMenuTabIndex);
-
-    // 删除对应的 widget
-    if (widget) {
-        widget->deleteLater();
     }
 }
--- src/ui/session/SessionTabWidget.h
+++ src/ui/session/SessionTabWidget.h
@@ -13,7 +13,6 @@ public:
 private slots:
     void showTabContextMenu(const QPoint &pos);
     void renameTab();
-    void closeTab();
 
 private:
     int m_contextMenuTabIndex = -1;  // 记录右键点击的 tab 索引
