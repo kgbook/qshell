b2dec1605d4ca3ed484a398bf090c263c2179b8b
qiushao <qiushaox@gmail.com>
2026-02-02 22:37:17 +0800

    添加 linux 打包 deb 配置

---
+++ scripts/linux-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
+cd ${SCRIPT_DIR}/..
+
+rm -rf build
+cmake -B build -S .
+cmake --build build -j8
+cmake --build build --target package
