c5127d6d267983a3546db06fe62b5f32e4c35aed
qiushao <qiushaox@gmail.com>
2026-02-03 14:44:24 +0800

    打包上传 linux deb 到 release

--- scripts/linux-build.sh
+++ scripts/linux-build.sh
@@ -3,7 +3,18 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 cd ${SCRIPT_DIR}/..
 
+# 优先使用传入参数，否则从 git tag 获取，最后使用默认值
+if [ -n "$1" ]; then
+    VERSION="$1"
+elif git describe --tags --abbrev=0 &>/dev/null; then
+    VERSION=$(git describe --tags --abbrev=0 | sed 's/^v//')
+else
+    VERSION="1.0.0"
+fi
+
+echo "Building version: ${VERSION}"
+
 rm -rf build
-cmake -B build -S .
+cmake -B build -S . -DAPP_VERSION="${VERSION}"
 cmake --build build -j8
 cmake --build build --target package
