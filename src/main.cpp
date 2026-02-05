c2070b8844f16056b9b054301d2fddb2d18c211c
qiushao <qiushaox@gmail.com>
2026-02-05 11:48:07 +0800

    集成 lua 运行时

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,13 @@
 #include <cstdio>
 #endif
 
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+}
+#include <sol/sol.hpp>
+
 static void showConsole(bool show)
 {
 #ifdef _WIN32
@@ -27,12 +34,34 @@ static void showConsole(bool show)
 #endif
 }
 
+static void luatest() {
+    // 测试 Lua 集成
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    lua.script(R"(
+        print("Hello from Lua " .. _VERSION)
+    )");
+
+    // 测试 Qt 与 Lua 交互
+    lua.set_function("qtVersion", []() {
+        return qVersion();
+    });
+
+    lua.script(R"(
+        print("Qt version: " .. qtVersion())
+    )");
+
+    qDebug() << "Lua integration successful!";
+}
+
 int main(int argc, char *argv[])
 {
     Q_INIT_RESOURCE(qtermwidget);
     QApplication a(argc, argv);
     showConsole(ConfigManager::instance()->globalSettings().debug);
     qDebug() << "style list:" << QStyleFactory::keys();
+    luatest();
     QApplication::setStyle(QStyleFactory::create("Fusion"));
     MainWindow w;
     w.show();
